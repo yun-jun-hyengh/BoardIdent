22ac511d185d2944fee9a5b16fdb7c7d190f0225
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-03-24 21:22:47 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/") // 현재경로로 접근 
+	public String index() {
+		return "index"; // 
+	}
+	
+	public String a() {
+		return "";
+	}
+
+}
